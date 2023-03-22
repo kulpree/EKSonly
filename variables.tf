fd7d182c9a2c4ebdb6459228fc57a38ea44643ce
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2023-03-21 17:57:59 -0700

    eksonlycode1

--- variables.tf
+++ variables.tf
@@ -4,12 +4,12 @@
   variable "consul_cidr_block1" {}
   variable "consul_cidr_block2" {}
   variable "consul_cidr_block3" {}
-  variable "cts_cidr_block" {}
-  variable "service_cidr_block" {}
+  #variable "cts_cidr_block" {}
+  #variable "service_cidr_block" {}
   #variable "availability_zone" {}
   
-  variable "instance_type" {}
-  variable "key_name" {}
+  #variable "instance_type" {}
+  #variable "key_name" {}
 
   #variable "ami" {}
   #variable "instance_type_agents" {}
