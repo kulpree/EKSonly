d5bfdbcafb3ed9228085fe7e59f81a576ff738b1
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2023-03-22 20:35:34 -0700

    lets try adding the csi driver

--- eksmain.tf
+++ eksmain.tf
@@ -194,7 +194,7 @@ resource "aws_security_group" "reinvent_sg" {
   tags = local.common_tags
 }
 
-#EKS_Cluster
+################################################EKS_Cluster#######################
 resource "aws_eks_cluster" "reinvent" {
   name     = "reinvent"
   role_arn = aws_iam_role.reinvent.arn
@@ -337,3 +337,31 @@ resource "aws_launch_template" "reinvent-launch-template" {
     }
   }
 }
+
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-reinvent"
+  provider_url                  = replace(data.aws_eks_cluster.eks.identity.0.oidc.0.issuer, "https://", "")
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "reinvent" {
+  cluster_name             = aws_eks_cluster.reinvent.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.5.2-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
+}
+
+
