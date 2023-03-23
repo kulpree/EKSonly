e89a53e09bb7ebeead19ebeca7945267cc2afbcd
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2023-03-22 20:38:08 -0700

    typo for csi config

--- eksmain.tf
+++ eksmain.tf
@@ -352,7 +352,7 @@ module "irsa-ebs-csi" {
 
   create_role                   = true
   role_name                     = "AmazonEKSTFEBSCSIRole-reinvent"
-  provider_url                  = replace(data.aws_eks_cluster.eks.identity.0.oidc.0.issuer, "https://", "")
+  provider_url                  = replace(data.aws_eks_cluster.reinvent.identity.0.oidc.0.issuer, "https://", "")
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
