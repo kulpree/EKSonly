bd840f3e7030a89dcd89e154d40061015c8a1456
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2023-07-17 17:35:10 -0700

    adding in required_providers instead

--- provider.tf
+++ provider.tf
@@ -1,8 +1,16 @@
 provider "aws" {
-  version = ">= 4.0"
+  #version = ">= 4.0"
   region  = var.region
   #access_key = "AWS_ACCESS_KEY_ID"
   #secret_key = "AWS_SECRET_ACCESS_KEY"
   #token = "AWS_SESSION_TOKEN"
 }
 
+terraform {
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
